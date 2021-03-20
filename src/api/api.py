4a611f4f1cf4d75d923db533670de49714631116
lombold <colin.disler@gmail.com>
2021-03-20 21:58:13 +0100

    WIP: Adds API to load the video
    dito

--- src/api/api.py
+++ src/api/api.py
@@ -1,6 +1,7 @@
 import json
 import os
-from flask import Flask
+from flask import Flask, send_file, send_from_directory, safe_join, abort
+
 from flask_cors import CORS
 
 from analyser import accident_detector
@@ -15,9 +16,29 @@ def status():
     return "UP!"
 
 
+@app.route('/user/<id>/climbs/<climb_id>')
+def get_climb(id, climb_id):
+    return {
+        'climb_id': climb_id,
+        'user_id': id,
+        'user_name': 'Max Muster',
+        'video_url': f'/user/{id}/climbs/{climb_id}/video'
+    }
+
+
+@app.route('/user/<id>/climbs/<climb_id>/video')
+def get_climb_video(id, climb_id):
+    print(app.instance_path)
+    filename = os.path.join(app.root_path, 'resources', 'vertical', 'Straight Wall (vertical).mp4')
+    # try:
+    return send_file(filename, as_attachment=True)
+    # except FileNotFoundError:
+    #     abort(404)
+
+
 @app.route('/user/<id>/climbs/<climb_id>/falls')
 def get_fall(id, climb_id):
-    with open('resources/vertical/40587108-e1a8-56ae-8c7f-1853f009b7c6.json') as f:
+    with open('./resources/vertical/40587108-e1a8-56ae-8c7f-1853f009b7c6.json') as f:
         data = json.load(f)
 
     detector = accident_detector.AccidentDetector(data)
