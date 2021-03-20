2f3398e3ef9bb40bb59d6647c56ac0fe1a6d069c
Simon Schwyter <simon.schwyter@students.fhnw.ch>
2021-03-20 15:30:50 +0100

    added i18n

--- src/climber-frontend/src/index.tsx
+++ src/climber-frontend/src/index.tsx
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
+
+i18next.init({
+  interpolation: { escapeValue: false }, 
+  react: {
+  useSuspense: false
+  } // React already does escaping
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <I18nextProvider i18n={i18next}>
+      <App />
+    </I18nextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
