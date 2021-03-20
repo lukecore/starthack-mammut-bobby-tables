65bd7ddc3571fa5afd3d862807d4349f87fb02c9
Lukas_Kern <lukaskern5@gmail.com>
2021-03-20 16:33:47 +0100

    added 3d plotting

--- src/movement-calculator/movement-calculator.py
+++ src/movement-calculator/movement-calculator.py
@@ -1,5 +1,7 @@
 import h5py as h5
+from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
+import numpy as np
 
 rawData = h5.File("../../resources/vertical/40587108-e1a8-56ae-8c7f-1853f009b7c6_raw.h5", "r")
 
@@ -12,13 +14,32 @@ ur_LH = []
 dp_LH = []
 timestamps_LH = []
 
+# for value in acc_LH:
+#     ap_LH.append(value['AP'])
+#     ur_LH.append(value['UR'])
+#     dp_LH.append(value['DP'])
+#     timestamps_LH.append(value['timestamp'])
+#
+# plt.plot(timestamps_LH, ap_LH, 'r', timestamps_LH, ur_LH, 'b', timestamps_LH, dp_LH, 'g')
+# plt.ylabel('AP Lefthand')
+# plt.xlabel('time')
+# plt.show()
+
 for value in acc_LH:
     ap_LH.append(value['AP'])
     ur_LH.append(value['UR'])
     dp_LH.append(value['DP'])
     timestamps_LH.append(value['timestamp'])
 
-plt.plot(timestamps_LH, ap_LH, 'r', timestamps_LH, ur_LH, 'b', timestamps_LH, dp_LH, 'g')
-plt.ylabel('AP Lefthand')
-plt.xlabel('time')
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+
+# Data for a three-dimensional line
+# zline = np.linspace(0, 15, 1000)
+# xline = np.sin(zline)
+# yline = np.cos(zline)
+ax.plot3D(ur_LH, dp_LH, ap_LH, 'gray')
+ax.scatter(ur_LH, dp_LH, ap_LH, 'b')
 plt.show()
+
+
