7c67ab84646a8c82464643421bf933fd96342e6b
Simon Schwyter <simon.schwyter@students.fhnw.ch>
2021-03-20 12:01:26 +0100

    added frontent with basic chart

---
+++ src/climber-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import Scatterplot from './components/Scatterplot/Scatterplot';
+
+function App() {
+  return (
+    <div className="App">
+      <Scatterplot></Scatterplot>
+    </div>
+  );
+}
+
+export default App;
