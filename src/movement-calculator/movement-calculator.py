d33f6a3cd519505072854317358638c1dcba54e4
Lukas_Kern <lukaskern5@gmail.com>
2021-03-20 15:02:22 +0100

    added h5 support and first simple plot of the dataset acc_LH

---
+++ src/movement-calculator/movement-calculator.py
@@ -0,0 +1,24 @@
+import h5py as h5
+import matplotlib.pyplot as plt
+
+rawData = h5.File("../../resources/vertical/40587108-e1a8-56ae-8c7f-1853f009b7c6_raw.h5", "r")
+
+print(list(rawData.keys()))
+
+acc_LH = rawData['/acc_LH']
+
+ap_LH = []
+ur_LH = []
+dp_LH = []
+timestamps_LH = []
+
+for value in acc_LH:
+    ap_LH.append(value['AP'])
+    ur_LH.append(value['UR'])
+    dp_LH.append(value['DP'])
+    timestamps_LH.append(value['timestamp'])
+
+plt.plot(timestamps_LH, ap_LH, 'r', timestamps_LH, ur_LH, 'b', timestamps_LH, dp_LH, 'g')
+plt.ylabel('AP Lefthand')
+plt.xlabel('time')
+plt.show()
